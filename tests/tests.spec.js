57dc46ca9116d8ff3178eed1edf6cf3ab706d4a9
Aksana <aksa.hleb@gmail.com>
2026-01-28 20:00:25 -0600

    Finish register user test

--- tests/tests.spec.js
+++ tests/tests.spec.js
@@ -19,7 +19,7 @@ test.describe("Tests", () => {
     const nameInputSignUpField = page.locator("form[action = '/signup'] input[name = 'name']");
     await nameInputSignUpField.fill("Aksana");
     const emailInputSignUpField = page.locator("form[action = '/signup'] input[name = 'email']");
-    await emailInputSignUpField.fill("aksana11@gmail.com");
+    await emailInputSignUpField.fill("aksana112@gmail.com");
     const signUpButton = page.locator("form[action = '/signup'] button");
     await signUpButton.click();
     await expect(page.getByRole('heading', { name: 'Enter Account Information' })).toBeVisible();
@@ -35,7 +35,36 @@ test.describe("Tests", () => {
     const firstNameInput = page.locator('input[name="first_name"]');
     await firstNameInput.fill("Aksana");
     await expect(firstNameInput).toHaveValue("Aksana");
-
+    const lastNameInput = page.locator('input[name="last_name"]');
+    await lastNameInput.fill("Hlebik");
+    await expect(lastNameInput).toHaveValue("Hlebik");
+    const companyInput = page.locator('input[name="company"]');
+    await companyInput.fill("WWW");
+    await expect(companyInput).toHaveValue("WWW");
+    const addressInput = page.locator('input[name="address1"]');
+    await addressInput.fill("111 ABC Ave");
+    await expect(addressInput).toHaveValue("111 ABC Ave");
+    const address2Input = page.locator('input[name="address2"]');
+    await address2Input.fill("apt 111");
+    await expect(address2Input).toHaveValue("apt 111");
+    await page.selectOption('select[name="country"]', 'United States');
+    await expect(page.locator('select[name="country"]')).toHaveValue('United States');
+    const stateInput = page.locator('input[name="state"]');
+    await stateInput.fill("Texas");
+    await expect(stateInput).toHaveValue("Texas");
+    const cityInput = page.locator('input[name="city"]');
+    await cityInput.fill("Houston");
+    await expect(cityInput).toHaveValue("Houston");
+    const zipcodeInput = page.locator('input[name="zipcode"]');
+    await zipcodeInput.fill("78787");
+    await expect(zipcodeInput).toHaveValue("78787");
+    const mobileNumberInput = page.locator('input[name="mobile_number"]');
+    await mobileNumberInput.fill("512-512-5522");
+    await expect(mobileNumberInput).toHaveValue("512-512-5522");
+    const createAccountButton = page.locator('button[data-qa="create-account"]');
+    createAccountButton.click();
+    const accountCreatedHeading = page.locator('h2[data-qa="account-created"]');
+    await expect(accountCreatedHeading).toBeVisible();
   });
 
   test("Login user with correct email and password", async ({ page }) => {
@@ -44,13 +73,13 @@ test.describe("Tests", () => {
     await expect(page).toHaveURL('https://www.automationexercise.com/login');
     await expect(page.getByRole('heading', { name: 'Login to your account' })).toBeVisible();
     const emailInputField = page.locator("form[action = '/login'] input[type = 'email']");
-    await emailInputField.fill("aksana11@gmail.com");
+    await emailInputField.fill("aksana112@gmail.com");
     const passwordInputField = page.locator("form[action = '/login'] input[type = 'password']");
     await passwordInputField.fill("12345");
     const loginButton = page.locator("form[action = '/login'] button");
     await loginButton.click();
     const loggedInText = page.locator('.navbar-nav li:last-child a');
-    await expect(loggedInText).toHaveText(' Logged in as Aksana Hlebik')
+    await expect(loggedInText).toHaveText('Logged in as Aksana Hlebik')
     const deleteAccountButton = page.locator('.navbar-nav li:nth-child(5)');
     await deleteAccountButton.click();
     await expect(page.locator('h2 b')).toHaveText('ACCOUNT DELETED!');
