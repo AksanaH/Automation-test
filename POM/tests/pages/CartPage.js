0537c8f67662336e4e6e745ade90043b539aa141
Aksana <aksa.hleb@gmail.com>
2026-02-19 21:45:58 -0600

    Add Subscription tests for home page and for Cart page

--- POM/tests/pages/CartPage.js
+++ POM/tests/pages/CartPage.js
@@ -1,50 +1,36 @@
 import { expect } from "@playwright/test";
 import { BasePage } from "./BasePage";
 
-export class HomePage extends BasePage {
+export class CartPage extends BasePage {
     constructor(page) {
         super(page);
-        this.heading = page.getByRole("heading", { name: "AutomationExercise" });
-        this.signupLoginLink = page.getByRole("link", { name: /Signup \/ Login/i });
         this.subscriptionTitle = page.getByRole("heading", { name: "SUBSCRIPTION" });
         this.subscriptionEmail = page.locator("#susbscribe_email");
         this.subscriptionArrowBtn = page.locator("#subscribe");
         this.subscriptionSuccess = page.locator("#success-subscribe");
-        this.cartLink = page.getByRole("link", { name: "Cart" });
-    }
 
-    async assertLoaded() {
-        await expect(this.heading).toBeVisible();
-    }
-
-    async openSignupLogin() {
-        await this.signupLoginLink.click();
     }
 
     async scrollToSubscription() {
         await this.subscriptionTitle.scrollIntoViewIfNeeded();
     }
 
-    async assertSubscriptionVisible() {
+    async assertSubscriptionInCartPageVisible() {
         await this.scrollToSubscription();
         await expect(this.subscriptionTitle).toBeVisible();
     }
 
-    async subscribe(email) {
+    async enterEmail(email) {
         await this.scrollToSubscription();
         await this.subscriptionEmail.fill(email);
         await this.subscriptionArrowBtn.click();
     }
 
-    async assertSubscriptionSuccess() {
+    async assertSuccessMessage() {
         // success блок появляется внутри #success-subscribe
         await expect(this.subscriptionSuccess).toBeVisible({ timeout: 15000 });
         await expect(this.subscriptionSuccess).toContainText(
             "You have been successfully subscribed!"
         );
-    }
-
-    async openCart() {
-        await this.cartLink.click();
     }
 }
